e05b92b2c88837977d673a91b2f2ec12b1ee4da6
Lucas John Jacobson <ljacobson64@gmail.com>
2016-10-10 17:14:50 -0500

    Fix some gfortran compile issues

--- regenerate_patch.sh
+++ regenerate_patch.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
 mkdir -p patch
+rm -f temp
 
-rm -f temp
-diff -rN "--unified=0" MCNP_CODE/MCNP5/Source MCNP_CODE/MCNP5/Source_new > temp
-sed -e "s/.F90\t.*/.F90/" temp > patch/mcnp.patch.5.1.60
-rm -f temp
-diff -rN "--unified=0" MCNP_CODE/MCNP6/Source MCNP_CODE/MCNP6/Source_new > temp
-sed -e "s/.F90\t.*/.F90/" temp > patch/mcnp.patch.6.1
-rm -f temp
-diff -rN "--unified=0" MCNP_CODE/MCNP611/Source MCNP_CODE/MCNP611/Source_new > temp
-sed -e "s/.F90\t.*/.F90/" temp > patch/mcnp.patch.6.1.1
-rm -f temp
+folders=( MCNP5 MCNP6 MCNP611 )
+versions=( 5.1.60 6.1.0 6.1.1 )
+
+for i in $(seq 0 2); do
+  folder=${folders[$i]}
+  version=${versions[$i]}
+  mv MCNP_CODE/${folder}/Source      MCNP_CODE/${folder}/Source_new
+  mv MCNP_CODE/${folder}/Source_orig MCNP_CODE/${folder}/Source
+  diff -rN "--unified=0" MCNP_CODE/${folder}/Source MCNP_CODE/${folder}/Source_new > temp
+  mv MCNP_CODE/${folder}/Source      MCNP_CODE/${folder}/Source_orig
+  mv MCNP_CODE/${folder}/Source_new  MCNP_CODE/${folder}/Source
+  sed -e "s/.F90\t.*/.F90/" temp > patch/mcnp.patch.${version}
+  rm -f temp
+done
