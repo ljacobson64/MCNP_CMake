bfbd0bf8e749d4a5b44ff4f11931badbbebaa979
Lucas John Jacobson <ljacobson64@gmail.com>
2017-03-15 16:44:34 -0500

    Update build.script

--- build.sh
+++ build.sh
@@ -1,49 +1,54 @@
 #!/bin/bash
 
+set -e
+
+clear
+clear
+
 jobs=8
-COMPILER=intel-17.0.0
-MPI=openmpi-1.10.4
-INTEL_ROOT=/opt/intel
 
-if   [[ ${COMPILER} == "gcc"*   ]]; then COMP=gcc
-elif [[ ${COMPILER} == "intel"* ]]; then COMP=intel
-fi
+compiler=gcc-6
+#compiler=intel-17
 
-if   [[ ${COMPILER} == intel-12.1.0 ]]; then INTEL_ROOT=${INTEL_ROOT}/composer_xe_2011_sp1.13.367
-elif [[ ${COMPILER} == intel-13.1.0 ]]; then INTEL_ROOT=${INTEL_ROOT}/composer_xe_2013.5.192
-elif [[ ${COMPILER} == intel-14.0.4 ]]; then INTEL_ROOT=${INTEL_ROOT}/composer_xe_2013_sp1.6.214
-elif [[ ${COMPILER} == intel-15.0.6 ]]; then INTEL_ROOT=${INTEL_ROOT}/composer_xe_2015.6.233
-elif [[ ${COMPILER} == intel-16.0.4 ]]; then INTEL_ROOT=${INTEL_ROOT}/compilers_and_libraries_2016.4.258
-elif [[ ${COMPILER} == intel-17.0.0 ]]; then INTEL_ROOT=${INTEL_ROOT}/compilers_and_libraries_2017.0.098
-fi
+openmpi_version=1.10.5
 
-if   [[ ${COMP} == gcc   ]]; then MPI_ROOT=/home/lucas/opt-gcc/${MPI}
-elif [[ ${COMP} == intel ]]; then MPI_ROOT=/home/lucas/opt-intel/${MPI}
-fi
+orig_dir=${PWD}
+install_dir=${HOME}/opt/${compiler}
+build_dir=${orig_dir}/bld
+install_prefix=${orig_dir}
 
-#source ${INTEL_ROOT}/linux/bin/compilervars.sh intel64
+if [[ ${compiler} == "intel"* ]]; then
+  if   [[ ${compiler} == "intel-12" ]]; then intel_root=/opt/intel/composer_xe_2011_sp1.13.367
+  elif [[ ${compiler} == "intel-13" ]]; then intel_root=/opt/intel/composer_xe_2013.5.192
+  elif [[ ${compiler} == "intel-14" ]]; then intel_root=/opt/intel/composer_xe_2013_sp1.6.214
+  elif [[ ${compiler} == "intel-15" ]]; then intel_root=/opt/intel/composer_xe_2015.6.233
+  elif [[ ${compiler} == "intel-16" ]]; then intel_root=/opt/intel/compilers_and_libraries_2016.4.258/linux
+  elif [[ ${compiler} == "intel-17" ]]; then intel_root=/opt/intel/compilers_and_libraries_2017.0.098/linux
+  fi
 
-export            PATH=${INTEL_ROOT}/linux/bin/intel64:${PATH}
-export LD_LIBRARY_PATH=${INTEL_ROOT}/linux/compiler/lib/intel64:${PATH}
-export            PATH=${MPI_ROOT}/bin:${PATH}
-export LD_LIBRARY_PATH=${MPI_ROOT}/lib:${LD_LIBRARY_PATH}
+  export PATH=${intel_root}/bin/intel64:${PATH}
+  export LD_LIBRARY_PATH=${intel_root}/compiler/lib/intel64:${LD_LIBRARY_PATH}
 
-CC=/usr/bin/gcc
-CXX=/usr/bin/g++
-if [[ ${COMP} == gcc ]]; then
-  #CC=/usr/bin/gcc
-  #CXX=/usr/bin/g++
-  FC=/usr/bin/gfortran
-elif [[ ${COMP} == intel ]]; then
-  #CC=`which icc`
-  #CXX=`which icpc`
+  CC=`which icc`
+  CXX=`which icpc`
   FC=`which ifort`
+elif [[ ${compiler} == "gcc"* ]]; then
+  CC=`which gcc`
+  CXX=`which g++`
+  FC=`which gfortran`
+else
+  exit
 fi
+
+mpi_root=${install_dir}/openmpi-${openmpi_version}
+
+export PATH=${mpi_root}/bin:${PATH}
+export LD_LIBRARY_PATH=${mpi_root}/lib:${LD_LIBRARY_PATH}
 
 cmake_string=
 
 cmake_string+=" "-DBUILD_MCNP5=ON
-cmake_string+=" "-DBUILD_MCNP6=ON
+#cmake_string+=" "-DBUILD_MCNP6=ON
 cmake_string+=" "-DBUILD_MCNP611=ON
 #cmake_string+=" "-DBUILD_MCNPX=ON
 
@@ -52,18 +57,31 @@ cmake_string+=" "-DOPENMP_BUILD=ON
 cmake_string+=" "-DMPI_BUILD=ON
 
 #cmake_string+=" "-DSTATIC_LIB=ON
+
 #cmake_string+=" "-DCMAKE_BUILD_TYPE=Debug
+#cmake_string+=" "-DCMAKE_BUILD_TYPE=Release
+
 cmake_string+=" "-DCMAKE_C_COMPILER=${CC}
 cmake_string+=" "-DCMAKE_CXX_COMPILER=${CXX}
 cmake_string+=" "-DCMAKE_Fortran_COMPILER=${FC}
-cmake_string+=" "-DCMAKE_INSTALL_PREFIX=/home/lucas/MCNP_CMake
+cmake_string+=" "-DCMAKE_INSTALL_PREFIX=${install_prefix}
 
-#rm -rf bin
-rm -rf bld
-mkdir bld
-cd bld
-cmake ../src $cmake_string
+log_file=${orig_dir}/install_${compiler}.log
+
+rm -rf ${build_dir}
+rm -rf bin
+mkdir -p ${build_dir}
+cd ${build_dir}
+
+which cmake
+cmake --version
+
+cmake ${orig_dir}/src ${cmake_string}
 make -j ${jobs}
-#make -j ${jobs} VERBOSE=1
 make install
+
+#/usr/bin/time -v cmake ${orig_dir}/src ${cmake_string} 2>&1 | tee ${log_file}
+#/usr/bin/time -v make -j ${jobs} VERBOSE=1 2>&1 | tee -a ${log_file}
+#/usr/bin/time -v make install 2>&1 | tee -a ${log_file}
+
 cd ..
