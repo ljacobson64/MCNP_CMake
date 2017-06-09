17f57bb8ea2fa018bfbd2a7957f015b1557058a0
Lucas John Jacobson <ljacobson64@gmail.com>
2017-06-09 14:45:06 -0500

    Remove MCNP_CODE dir

--- mcnp_source.sh
+++ mcnp_source.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-cd MCNP_CODE
 rm -rf */Source */Source_orig
 tar -xzvf ${dist_dir}/mcnp/mcnp5.1.60_source.tar.gz
 tar -xzvf ${dist_dir}/mcnp/mcnp6.1_source.tar.gz
@@ -8,7 +7,6 @@ tar -xzvf ${dist_dir}/mcnp/mcnp6.1.1beta_source.tar.gz
 cp -r MCNP5/Source MCNP5/Source_orig
 cp -r MCNP6/Source MCNP6/Source_orig
 cp -r MCNP611/Source MCNP611/Source_orig
-cd ..
 patch -p0 < patch/mcnp.5.1.60.patch
 patch -p0 < patch/mcnp.6.1.patch
 patch -p0 < patch/mcnp.6.1.1beta.patch
--- regenerate_patch.sh
+++ regenerate_patch.sh
@@ -9,11 +9,11 @@ versions=( 5.1.60 6.1 6.1.1beta )
 for i in $(seq 0 2); do
   folder=${folders[$i]}
   version=${versions[$i]}
-  mv MCNP_CODE/${folder}/Source      MCNP_CODE/${folder}/Source_new
-  mv MCNP_CODE/${folder}/Source_orig MCNP_CODE/${folder}/Source
-  diff -rN "--unified=0" MCNP_CODE/${folder}/Source MCNP_CODE/${folder}/Source_new > temp
-  mv MCNP_CODE/${folder}/Source      MCNP_CODE/${folder}/Source_orig
-  mv MCNP_CODE/${folder}/Source_new  MCNP_CODE/${folder}/Source
+  mv ${folder}/Source      ${folder}/Source_new
+  mv ${folder}/Source_orig ${folder}/Source
+  diff -rN "--unified=0" ${folder}/Source ${folder}/Source_new > temp
+  mv ${folder}/Source      ${folder}/Source_orig
+  mv ${folder}/Source_new  ${folder}/Source
   sed -e "s/.F90\t.*/.F90/" -e "s/.inc\t.*/.inc/" temp > patch/mcnp.${version}.patch
   rm -f temp
 done
