432702395ad196804f30f5e121630d4672970f7d
Lucas John Jacobson <ljacobson64@gmail.com>
2025-06-03 12:16:50 -0500

    Fix gfortran-15 errors

--- mcnp_source.sh
+++ mcnp_source.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
+set -e
+
 rm -rf */Source */Source_orig
-tar -xzvf ${dist_dir}/mcnp/mcnp514-source.tar.gz
-tar -xzvf ${dist_dir}/mcnp/mcnp515-source.tar.gz
-tar -xzvf ${dist_dir}/mcnp/mcnp516-source.tar.gz
-tar -xzvf ${dist_dir}/mcnp/mcnpx27-source.tar.gz
-tar -xzvf ${dist_dir}/mcnp/mcnp602-source.tar.gz
-tar -xzvf ${dist_dir}/mcnp/mcnp610-source.tar.gz
-tar -xzvf ${dist_dir}/mcnp/mcnp611-source.tar.gz
-tar -xzvf ${dist_dir}/mcnp/mcnp620-source.tar.gz
+
+tar -xzvf ${dist_dir}/mcnp514-source.tar.gz
+tar -xzvf ${dist_dir}/mcnp515-source.tar.gz
+tar -xzvf ${dist_dir}/mcnp516-source.tar.gz
+tar -xzvf ${dist_dir}/mcnpx27-source.tar.gz
+tar -xzvf ${dist_dir}/mcnp602-source.tar.gz
+tar -xzvf ${dist_dir}/mcnp610-source.tar.gz
+tar -xzvf ${dist_dir}/mcnp611-source.tar.gz
+tar -xzvf ${dist_dir}/mcnp620-source.tar.gz
+
 cp -r MCNP514/Source MCNP514/Source_orig
 cp -r MCNP515/Source MCNP515/Source_orig
 cp -r MCNP516/Source MCNP516/Source_orig
@@ -17,4 +21,5 @@ cp -r MCNP602/Source MCNP602/Source_orig
 cp -r MCNP610/Source MCNP610/Source_orig
 cp -r MCNP611/Source MCNP611/Source_orig
 cp -r MCNP620/Source MCNP620/Source_orig
+
 ./patch.sh
